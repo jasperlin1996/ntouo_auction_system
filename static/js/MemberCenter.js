2f78d644c29ee3c259278ea4bc5df2d0cf6c12ae
im99014 <egg99014@gmail.com>
2019-05-14 22:55:43 +0800

    add MemberCenter and Product(js not fininsh)

--- static/js/MemberCenter.js
+++ static/js/MemberCenter.js
@@ -3,21 +3,21 @@ var word = 0;
 // 顯示個人資訊頁面
 function person() {
     var temp = "<table><tr><td>帳號:</td><td>";
-    temp += '123456'; // {{ account }};
+    temp += {{ account }};
     temp += '</td></tr><tr><td>密碼:</td><td id="pass">********<input type="button" onclick="showPassword()" value="查看"></td></tr><tr><td>姓名:</td><td>';
-    temp += '王金魚'; // {{ user_name }};
+    temp += {{ user_name }};
     temp += '</td></tr><tr><td>海大email</td><td>';
-    temp += '123546@ntou'; // {{ ntou_email }};
+    temp += {{ ntou_email }};
     temp += '</td></tr><tr><td>電話:</td><td>';
-    temp += '031659'; // {{ phone }};
+    temp += {{ phone }};
     temp += '</td></tr><tr><td>地址:</td><td>';
-    temp += '基隆市中正區北寧路2號'; // {{ address }};
+    temp += {{ address }};
     temp += '</td></tr><tr><td>其他聯絡方式:</td><td><textarea rows="5" cols="50" readonly>';
-    temp += 'line:165461'; // {{ contact }};
+    temp += {{ contact }};
     temp += '</textarea></td></tr><tr><td>購買評價:</td><td>';
-    temp += '2.5'; // {{ buyer_rate }};
+    temp += {{ buyer_rate }};
     temp += '</td></tr><tr><td>販賣評價:</td><td>';
-    temp += '4.5'; // {{ seller_rate }};
+    temp += {{ seller_rate }};
     temp += '</td></tr><tr><td>第三方資訊</td><td>';
     // 第三方資訊顯示
     temp += '</td></tr><tr><td></td><td><input type="button" onclick="change()" value="修改"></td></tr></table>';
@@ -26,127 +26,127 @@ function person() {
 
 // 顯示追蹤中清單頁面
 function likes() {
-    //var items = {{ tracking_items | safe }};
+    var items = {{ tracking_items | safe }};
     var temp = "";
-    /*if (items.length == 0) {
+    if (items.length == 0) {
         temp = "<p>無追蹤中商品</p>";
     }
-    else {*/
+    else {
         temp = '<table id="obj">';
-        for (var i = 0; i < 10/*items.length*/; i++) {
+        for (var i = 0; i < items.length; i++) {
             temp += '<tr><td><a href="';
-            temp += '../templates/Product.html?'; //+ item[i].id
+            temp += '../templates/Product.html?' + item[i].id;
             temp += '"><img src="';
-            temp += '../static/img/cantFind.png';  // items[i].image;
+            temp += items[i].image;
             temp += '"></a></td><td><a href="';
-            temp += '../templates/Product.html?'; //+ item[i].id
-            temp += '">找不到商品';  // items[i].product_name;
+            temp += '../templates/Product.html?' + item[i].id;
+            temp += '">'; + items[i].product_name;
             temp += '</td></a></tr>';
         }
         temp += '</table>';
-    //}
+    }
     document.getElementById("content").innerHTML = temp;
 }
 
 // 顯示競標中清單頁面
 function buying() {
-    //var items = {{ bidding_items | safe }};
+    var items = {{ bidding_items | safe }};
     var temp = "";
-    /*if (items.length == 0) {
+    if (items.length == 0) {
         temp = "<p>無競標中商品</p>";
     }
-    else {*/
+    else {
         temp = '<table id="obj">';
-        for (var i = 0; i < 2/*items.length*/; i++) {
+        for (var i = 0; i < items.length; i++) {
             temp += '<tr><td><a href="';
-            temp += '../templates/Product.html?'; //+ item[i].id
+            temp += '../templates/Product.html?' + item[i].id;
             temp += '"><img src="';
-            temp += '../static/img/cantFind.png';  // items[i].image;
+            temp += items[i].image;
             temp += '"></a></td><td><a href="';
-            temp += '../templates/Product.html?'; //+ item[i].id
-            temp += '">找不到商品';  // items[i].product_name;
+            temp += '../templates/Product.html?' + item[i].id;
+            temp += '">' + items[i].product_name;
             temp += '</td></a></tr>';
         }
         temp += '</table>';
-    //}
+    }
     document.getElementById("content").innerHTML = temp;
 }
 
 // 顯示交易中清單頁面
 function dealing() {
-    //var items = {{ dealing_items | safe }};
+    var items = {{ dealing_items | safe }};
     var temp = "";
-    /*if (items.length == 0) {
+    if (items.length == 0) {
         temp = "<p>無交易中商品</p>";
-    }*/
-    //else {
+    }
+    else {
         temp = '<table id="obj">';
-        for (var i = 0; i < 2/*items.length*/; i++) {
+        for (var i = 0; i < items.length; i++) {
             temp += '<tr><td><a href="';
-            temp += '../templates/Product.html?'; //+ item[i].id
+            temp += '../templates/Product.html?' + item[i].id;
             temp += '"><img src="';
-            temp += '../static/img/cantFind.png';  // items[i].image;
+            temp += items[i].image;
             temp += '"></a></td><td><a href="';
-            temp += '../templates/Product.html?'; //+ item[i].id
-            temp += '">找不到商品';  // items[i].product_name;
+            temp += '../templates/Product.html?' + item[i].id;
+            temp += '">' + items[i].product_name;
             temp += '</td></a></tr>';
         }
         temp += '</table>';
-    //}
+    }
     document.getElementById("content").innerHTML = temp;
 }
 
 // 顯示販賣中清單頁面
 function selling() {
-    //var items = {{ onsale_items | safe }};
+    var items = {{ onsale_items | safe }};
     var temp = "";
-    /*if (items.length == 0) {
+    if (items.length == 0) {
         temp = "<p>無販賣中商品</p>";
-    }*/
-    //else {
+    }
+    else {
         temp = '<table id="obj">';
-        for (var i = 0; i < 2/*items.length*/; i++) {
+        for (var i = 0; i < items.length; i++) {
             temp += '<tr><td><a href="';
-            temp += '../templates/Product.html?&'; //+ item[i].id
+            temp += '../templates/Product.html?' + item[i].id;
             temp += '"><img src="';
-            temp += '../static/img/cantFind.png';  // items[i].image;
+            temp += items[i].image;
             temp += '"></a></td><td><a href="';
-            temp += '../templates/Product.html?&'; //+ item[i].id
-            temp += '">找不到商品';  // items[i].product_name;
+            temp += '../templates/Product.html?' + item[i].id
+            temp += '">' + items[i].product_name;
             temp += '</td></a></tr>';
         }
         temp += '</table>';
-    //}
+    }
     document.getElementById("content").innerHTML = temp;
 }
 
 // 顯示已完成清單頁面
 function finish() {
-    //var items = {{ done_items | safe }};
+    var items = {{ done_items | safe }};
     var temp = "";
-    /*if (items.length == 0) {
+    if (items.length == 0) {
         temp = "<p>無已完成商品</p>";
-    }*/
-    //else {
+    }
+    else {
         temp = '<table id="obj">';
-        for (var i = 0; i < 2/*items.length*/; i++) {
+        for (var i = 0; i < items.length; i++) {
             temp += '<tr><td><a href="';
-            temp += '../templates/Product.html?'; //+ item[i].id
+            temp += '../templates/Product.html?' + item[i].id
             temp += '"><img src="';
-            temp += '../static/img/cantFind.png';  // items[i].image;
+            temp += items[i].image;
             temp += '"></a></td><td><a href="';
-            temp += '../templates/Product.html?'; //+ item[i].id
-            temp += '">找不到商品';  // items[i].product_name;
+            temp += '../templates/Product.html?' + item[i].id;
+            temp += '">' + items[i].product_name;
             temp += '</td></a></tr>';
         }
         temp += '</table>';
-    //}
+    }
     document.getElementById("content").innerHTML = temp;
 }
 
 // 顯示密碼
 function showPassword() {
-    document.getElementById("pass").innerHTML = /* {{ password | safe }} + */'<input type="button" onclick="hidePassword()" value="隱藏">';
+    document.getElementById("pass").innerHTML = {{ password | safe }} + '<input type="button" onclick="hidePassword()" value="隱藏">';
 }
 
 // 隱藏密碼
@@ -157,24 +157,25 @@ function hidePassword() {
 // 更改個人資訊
 function change() {
     var temp = "<table><tr><td>帳號:</td><td>";
-    temp += '123456'; // {{ account | safe }};
-    temp += '</td></tr><tr><td>密碼:</td><td id="pass">********<input type="button" onclick="showPassword()" value="查看"></td></tr><tr><td>姓名:</td><td>';
-    temp += '<textarea rows="1" cols="30" id="user_name">王金魚</textarea>'; // {{ user_name | safe }};
+    temp += {{ account | safe }};
+    temp += '</td></tr><tr><td>密碼:</td><td>';
+    temp += '輸入密碼:<>';
+    temp += '</td></tr><tr><td>姓名:</td><td>';
+    temp += '<textarea rows="1" cols="30" name="user_name">' + {{ user_name | safe }} + '</textarea>';
     temp += '</td></tr><tr><td>海大email</td><td>';
-    temp += '123546@ntou'; // {{ ntou_email | safe }};
+    temp += {{ ntou_email | safe }};
     temp += '</td></tr><tr><td>電話:</td><td>';
-    temp += '<textarea rows="1" cols="30" id="phone">031659</textarea>'; // {{ phone | safe }};
+    temp += '<textarea rows="1" cols="30" name="phone">' + {{ phone | safe }} + '</textarea>';
     temp += '</td></tr><tr><td>地址:</td><td>';
-    temp += '<textarea rows="1" cols="30" id="address">基隆市中正區北寧路2號</textarea>'; // {{ address | safe }};
-    temp += '</td></tr><tr><td>其他聯絡方式:</td><td><textarea rows="5" cols="50">';
-    temp += 'line:165461'; // {{ contact | safe }};
+    temp += '<textarea rows="1" cols="30" name="address">' + {{ address | safe }} + '</textarea>'; // {{ address | safe }};
+    temp += '</td></tr><tr><td>其他聯絡方式:</td><td><textarea rows="5" cols="50" name="contact">';
+    temp += {{ contact | safe }};
     temp += '</textarea></td></tr><tr><td>購買評價:</td><td>';
-    temp += '2.5'; // {{ buyer_rate | safe }};
+    temp += {{ buyer_rate | safe }};
     temp += '</td></tr><tr><td>販賣評價:</td><td>';
-    temp += '4.5'; // {{ seller_rate | safe }};
+    temp += {{ seller_rate | safe }};
     temp += '</td></tr><tr><td>第三方資訊</td><td>';
     // 第三方資訊顯示
     temp += '</td></tr><tr><td></td><td><input type="submit" value="送出"><input type="button" onclick="person()" value="取消"></td></tr></table>';
     document.getElementById("content").innerHTML = temp;
 }
-
--- static/js/Product.js
+++ static/js/Product.js
@@ -8,19 +8,18 @@ function load() {
     }
     else {
         var array = url.split('?');
-        if (array.indexOf('&')!=-1) {
+        
+        var id = array[1];
+        var info = {{ get_product_info | safe }};
+        if ({{ request.session.idToken }} == info.saler) {
             canChange = 1;
         }
-        var id = array[1];
-        //var info = {{ 'get-product-info | safe }};
 
         var str = '';
-        str += '<table><tr><td>圖片:</td><td><img src="';
-        str += '../static/img/cantFind.png' // info.image
-        str += '"></td></tr><tr><td>名稱:</td><td>';
-        str += '找不到商品'; // info.product_name;
-        str += '</td></tr><td>狀態:</td><td>';
-        /*
+        str += '<table><tr><td>圖片:</td><td><img src="' + info.image + '"></td></tr>';
+        str += '"<tr><td>名稱:</td><td>' + info.product_name + '</td></tr>';
+        
+        str += '<tr><td>狀態:</td><td>';
         if (info.status == 0) {
             str += '待出售';
         }
@@ -36,40 +35,30 @@ function load() {
         else {
             str += '已下架';
         }
-        */
+
         str += '</td></tr><tr><td>直購/拍賣:</td><td>';
-        /*if (info.type == 0) { // 拍賣
-            str += '拍賣</td></tr><tr><td>直購價:</td><td>';
-            str += info.price;
-            str += '元</td></tr><tr><td>底價:</td><td>';
-            str += info.current_price;
-            str += '元</td></tr><tr><td>每標底價:</td><td>';
-            str += info.price_per_mark;
-        }*/
-        str += '拍賣</td></tr><tr><td>直購價:</td><td>1000元</td></tr>';
-        str += '<tr><td>底價:</td><td>1000元</td></tr>';
-        str += '<tr><td>每標底價:</td><td>1000元</td></tr>';
-        /*else { // 直購
-            str += '直購</td><tr><tr><td>直購價:</td><td>';
-            str += info.price;
-            str += '元</td></tr><tr><td>底價:</td><td>';
-            str += '元</td></tr><tr><td>每標底價:</td><td>';
-        }*/
-        str += '<tr><td>分類:</td><td>';
-        str += 'one'; // info.catagory;
-        str += '</td></tr><tr><td>敘述:</td><td>';
-        str += '124879684gj'; // info.description;
-        str += '</td></tr><tr><td>希望交易方式:</td><td>';
-        str += '..............'; // info.trading_method
-        str += '</td></tr><tr><td>結標日期:</td><td>';
-        str += '1999年12月31日'; // info.deadline
+        if (info.type == 0) { // 拍賣
+            str += '拍賣</td></tr>';
+            str += '<tr><td>直購價:</td><td>' + info.price + '元</td></tr>';
+            str += '<tr><td>底價:</td><td>' + info.current_price + '元</td></tr>';
+            str += '<tr><td>每標底價:</td><td>' + info.price_per_mark + '元</td></tr>';
+        }
+        else { // 直購
+            str += '直購</td><tr><tr><td>直購價:</td><td>' + info.price + '元</td></tr>';
+        }
+
+        str += '<tr><td>分類:</td><td>' + info.catagory + '</td></tr>';
+        str += '<tr><td>敘述:</td><td>' + info.description + '</td></tr>';
+        str += '<tr><td>賣家:</td><td>' + info.saler + '</td></tr>';
+        str += '<tr><td>希望交易方式:</td><td>' + info.trading_method + '</td></tr>';
+        str += '<tr><td>結標日期:</td><td>' + info.deadline + '</td></tr>';
+        
         if (canChange == 1) {
-            str += '</td></tr><tr><td></td><td>';
+            str += '<tr><td></td><td>';
             str += '<input type="button" onclick="change()" value="修改">';
-            str += '<input type="button" onclick="obtained()" value="下架">';
         }
         else {
-            str += '</td></tr><tr><td></td><td>';
+            str += '<tr><td></td><td>';
             str += '<input type="button" onclick="bid()" value="出價">';
             str += '<input type="button" onclick="buy()" value="直接購買">';
         }
@@ -80,55 +69,46 @@ function load() {
 
 function change() {
     console.log('change');
-    str = '<table><tr><td>圖片:</td><td><img src="';
-    str += '../static/img/cantFind.png"' // info.image
-    str += '></td><td><input id="image" type="file" accept="image/jpeg, image/png"';
-    str += '"></td></tr><tr><td>名稱:</td><td>';
-    str += '找不到商品'; // info.product_name;
-    str += '</td><td><input id="product_name" type="text">'
-    str += '</td></tr><tr><td>直購/拍賣:</td><td>';
-    /*if (info.type == 0) { // 拍賣
-        str += '拍賣</td><td>';
-        str += '<input id="sell" type="radio" name="buuStyle"/>直購';
-        str += '<input id="sale" type="radio" name="buyStyle" checked="true"/>拍賣';
-        str += '</td></tr><tr><td>直購價:</td><td>';
-        str += info.price;
-        str += '元</td></tr><tr><td>底價:</td><td>';
-        str += info.current_price;
-        str += '元</td></tr><tr><td>每標底價:</td><td>';
-        str += info.price_per_mark;
-    }*/
-    str += '拍賣</td><td>';
-    str += '<input id="sell" type="radio" name="buuStyle"/>直購';
-    str += '<input id="sale" type="radio" name="buyStyle" checked="true"/>拍賣';
-    str += '</td></tr><tr><td>直購價:</td><td>1000元</td>';
-    str += '<td><input id="price" type="text"></td></tr>';
-    str += '<tr><td>底價:</td><td>1000元</td>';
-    str += '<td><input id="current_price" type="text"></td></tr>';
-    str += '<tr><td>每標底價:</td><td>1000元</td>';
-    str += '<td><input id="price_per_mark" type="text></td></tr>';
-    /*else { // 直購
-        str += '直購</td><td>';
-        str += '<input id="sell" type="radio" name="buuStyle" checked="true"/>直購';
-        str += '<input id="sale" type="radio" name="buyStyle"/>拍賣';
-        str += '</td><tr><tr><td>直購價:</td><td>';
-        str += info.price;
-        str += '元</td></tr><tr><td>底價:</td><td>';
-        str += '元</td></tr><tr><td>每標底價:</td><td>';
-    }*/
-    str += '<tr><td>分類:</td><td>';
-    str += 'one'; // info.catagory;
-    str += '</td></tr><tr><td>敘述:</td><td>';
-    str += '124879684gj'; // info.description;
-    str += '</td>';
-    str += '<td><input id="description" type="text"></td></tr><tr><td>希望交易方式:</td><td>';
-    str += '..............'; // info.trading_method
-    str += '</td><td><input id="trading_method" type="text"></td>';
-    str += '</tr><tr><td>結標日期:</td><td>';
-    str += '1999年12月31日'; // info.deadline
-    str += '</td><td><input id="deadline" type="date"></td>';
-    str += '</tr><tr><td></td><td>';
-    str += '<input type="submit" onclick="send()" value="確認修改">';
+    str = '<table><tr><td>圖片:</td><td><img src="' + info.image + '"></td><td><input name="image" type="file" accept="image/jpeg, image/png"></td></tr>';
+    str += '<tr><td>名稱:</td><td>' + info.product_name + '</td><td><input name="product_name" type="text"></td></tr>';
+    
+    str += '<tr><td>狀態:</td><td>';
+    if (info.status == 0) {
+        str += '待出售';
+    }
+    else if (info.status == 1) {
+        str += '競標中';
+    }
+    else if (info.status == 2) {
+        str += '交易中';
+    }
+    else if (info.status == 3) {
+        str += '已賣出';
+    }
+    else {
+        str += '已下架';
+    }
+    str += '</td><td></td></tr>';
+
+    str += '<tr><td>直購/拍賣:</td><td>';
+    if (info.type == 0) { // 拍賣
+        str += '拍賣</td><td></td></tr>';
+        str += '<tr><td>直購價:</td><td>' + info.price + '元</td><td></td></tr>';
+        str += '<tr><td>底價:</td><td>' + info.current_price + '元</td><td></td></tr>';
+        str += '<tr><td>每標底價:</td><td>' + info.price_per_mark + '元</td><td></td></tr>';
+    }
+    else { // 直購
+        str += '直購</td><td></td></tr>';
+        str += '<tr><td>直購價:</td><td>' + info.price + '元</td><td></td></tr>';
+    }
+
+    str += '<tr><td>分類:</td><td>' + info.catagory + '</td></tr>';
+    str += '<tr><td>敘述:</td><td>' + info.description + '</td><td><input name="description" type="text"></td></tr>';
+    str += '<tr><td>賣家:</td><td>' + info.saler + '</td><td></td></tr>';
+    str += '<tr><td>希望交易方式:</td><td>' + info.trading_method + '</td><td><input name="trading_method" type="text"></td></tr>';
+    str += '<tr><td>結標日期:</td><td>' + info.deadline + '</td><td><input name="deadline" type="date"></td></tr>';
+    str += '<tr><td></td><td></td><td>';
+    str += '<input type="submit" value="確認修改">';
     str += '<input type="button" onclick="load()" value="取消">';
     str += '</td></tr></table>';
     document.getElementById("content").innerHTML = str;
@@ -139,13 +119,5 @@ function bid() {
 }
 
 function buy() {
-
-}
-
-function send() {
-
-}
-
-function obtained() {
 
 }
