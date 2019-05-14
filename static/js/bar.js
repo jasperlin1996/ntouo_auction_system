285674efca3057919b94402224bd7159a9842383
Andy <andy870622@gmail.com>
2019-05-14 23:18:57 +0800

    add google document

--- static/js/bar.js
+++ static/js/bar.js
@@ -21,10 +21,14 @@ function start() {
   }
 }
 
+function openCommand() {
+  window.open(' https://forms.gle/TwwdvBqdyqwR4TXf8 ', '', config = 'height=700,width=500');
+}
+
 var body = document.getElementsByTagName("body");
 var need =
   " <header class='header1'><img style='position:absolute;width:50px;height:50px;margin-left:10px;margin-top:5px;' src='./static/img/ntoulogo.png'><div style='margin-top:15px'><a id='sign'>海大拍賣</a><input type=button value='搜尋' disabled style='font-size:15px;'><input type='text' style='width:100px;'>" +
   "<span id='bar' class='bar' style='color:white; float: right;'><a onclick= index()" +
-  ">主頁面</a><a>會員中心</a><a>意見回饋</a><a>瀏覽紀錄</a><a onclick='signIn()'> 登入</a><a onclick='signUpString()'> 註冊</a></span></div></header>";
+  ">主頁面</a><a>會員中心</a><a onclick='openCommand()'>意見回饋</a><a>瀏覽紀錄</a><a onclick='signIn()'> 登入</a><a onclick='signUpString()'> 註冊</a></span></div></header>";
 
 $("body").prepend(need);
