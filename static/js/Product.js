a25d5e533b255df675737ea7b811977177081745
im99014 <egg99014@gmail.com>
2019-05-16 00:47:14 +0800

    MemberCenter, Product, Modify

--- static/js/Product.js
+++ static/js/Product.js
@@ -4,22 +4,28 @@ function load() {
     var canChange = 0;
     var url = location.href;
     if(url.indexOf('?')==-1) {
-        console.log("no product input");
+        alert("無商品!");
+        window.history.back();
     }
     else {
         var array = url.split('?');
-        
         var id = array[1];
-        var info = {{ product | safe }};
+
+        if (url.indexOf('&')!=-1) {
+            canChange = 1;
+        }
+        
         if ({{ request.session.idToken }} == info.saler) {
             canChange = 1;
         }
 
         var str = '';
-        str += '<table><tr><td>圖片:</td><td><img src="' + info.image + '"></td></tr>';
-        str += '"<tr><td>名稱:</td><td>' + info.product_name + '</td></tr>';
+        str += '<table><tr><td>圖片:</td><td><img src="';
+        str += info.image
+        str += '"></td></tr><tr><td>名稱:</td><td>';
+        str += info.product_name;
+        str += '</td></tr><td>狀態:</td><td>';
         
-        str += '<tr><td>狀態:</td><td>';
         if (info.status == 0) {
             str += '待出售';
         }
@@ -35,30 +41,35 @@ function load() {
         else {
             str += '已下架';
         }
-
+        
         str += '</td></tr><tr><td>直購/拍賣:</td><td>';
         if (info.type == 0) { // 拍賣
-            str += '拍賣</td></tr>';
-            str += '<tr><td>直購價:</td><td>' + info.price + '元</td></tr>';
-            str += '<tr><td>底價:</td><td>' + info.current_price + '元</td></tr>';
-            str += '<tr><td>每標底價:</td><td>' + info.price_per_mark + '元</td></tr>';
+            str += '拍賣</td></tr><tr><td>直購價:</td><td>';
+            str += info.price;
+            str += '元</td></tr><tr><td>底價:</td><td>';
+            str += info.current_price;
+            str += '元</td></tr><tr><td>每標底價:</td><td>';
+            str += info.price_per_mark;
         }
         else { // 直購
-            str += '直購</td><tr><tr><td>直購價:</td><td>' + info.price + '元</td></tr>';
+            str += '直購</td><tr><tr><td>直購價:</td><td>';
+            str += info.price;
+            str += '元</td></tr>';
         }
-
-        str += '<tr><td>分類:</td><td>' + info.catagory + '</td></tr>';
-        str += '<tr><td>敘述:</td><td>' + info.description + '</td></tr>';
-        str += '<tr><td>賣家:</td><td>' + info.saler + '</td></tr>';
-        str += '<tr><td>希望交易方式:</td><td>' + info.trading_method + '</td></tr>';
-        str += '<tr><td>結標日期:</td><td>' + info.deadline + '</td></tr>';
-        
+        str += '<tr><td>分類:</td><td>';
+        str += info.catagory;
+        str += '</td></tr><tr><td>敘述:</td><td>';
+        str += info.description;
+        str += '</td></tr><tr><td>希望交易方式:</td><td>';
+        str += info.trading_method
+        str += '</td></tr><tr><td>結標日期:</td><td>';
+        str += info.deadline
         if (canChange == 1) {
-            str += '<tr><td></td><td>';
-            str += '<input type="button" onclick="change()" value="修改">';
+            str += '</td></tr><tr><td></td><td>';
+            str += '<a href="../templates/Modify.html?' + id + '"><input type="button" value="修改"></a>';
         }
         else {
-            str += '<tr><td></td><td>';
+            str += '</td></tr><tr><td></td><td>';
             str += '<input type="button" onclick="bid()" value="出價">';
             str += '<input type="button" onclick="buy()" value="直接購買">';
         }
@@ -67,57 +78,12 @@ function load() {
     }
 }
 
-function change() {
-    console.log('change');
-    str = '<table><tr><td>圖片:</td><td><img src="' + info.image + '"></td><td><input name="image" type="file" accept="image/jpeg, image/png"></td></tr>';
-    str += '<tr><td>名稱:</td><td>' + info.product_name + '</td><td><input name="product_name" type="text"></td></tr>';
-    
-    str += '<tr><td>狀態:</td><td>';
-    if (info.status == 0) {
-        str += '待出售';
-    }
-    else if (info.status == 1) {
-        str += '競標中';
-    }
-    else if (info.status == 2) {
-        str += '交易中';
-    }
-    else if (info.status == 3) {
-        str += '已賣出';
-    }
-    else {
-        str += '已下架';
-    }
-    str += '</td><td></td></tr>';
-
-    str += '<tr><td>直購/拍賣:</td><td>';
-    if (info.type == 0) { // 拍賣
-        str += '拍賣</td><td></td></tr>';
-        str += '<tr><td>直購價:</td><td>' + info.price + '元</td><td></td></tr>';
-        str += '<tr><td>底價:</td><td>' + info.current_price + '元</td><td></td></tr>';
-        str += '<tr><td>每標底價:</td><td>' + info.price_per_mark + '元</td><td></td></tr>';
-    }
-    else { // 直購
-        str += '直購</td><td></td></tr>';
-        str += '<tr><td>直購價:</td><td>' + info.price + '元</td><td></td></tr>';
-    }
-
-    str += '<tr><td>分類:</td><td>' + info.catagory + '</td></tr>';
-    str += '<tr><td>敘述:</td><td>' + info.description + '</td><td><input name="description" type="text"></td></tr>';
-    str += '<tr><td>賣家:</td><td>' + info.saler + '</td><td></td></tr>';
-    str += '<tr><td>希望交易方式:</td><td>' + info.trading_method + '</td><td><input name="trading_method" type="text"></td></tr>';
-    str += '<tr><td>結標日期:</td><td>' + info.deadline + '</td><td><input name="deadline" type="date"></td></tr>';
-    str += '<tr><td></td><td></td><td>';
-    str += '<input type="submit" value="確認修改">';
-    str += '<input type="button" onclick="load()" value="取消">';
-    str += '</td></tr></table>';
-    document.getElementById("content").innerHTML = str;
-}
-
+// 出價
 function bid() {
 
 }
 
+// 直購
 function buy() {
 
 }
