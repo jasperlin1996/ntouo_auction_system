6410df2d045081672917b83b59467f39eaa8fe37
MSKN-Ho <olieho1210@gmail.com>
2019-05-21 15:58:55 +0800

    saparate scrips from html files (Using 'same name'.js)

---
+++ static/js/SignUp.js
@@ -0,0 +1,108 @@
+
+/*
+    checkData
+    表單確認後執行
+    確認輸入資料之正確性
+*/
+function checkData() {
+   
+    //紀錄輸入資料是否有誤
+    var isWrong = 0;
+
+    //提出input資料
+    var name = document.getElementById("nameBox").value;
+    var tele = document.getElementById("telBox").value;
+    var add = document.getElementById("addBox").value;
+    var sns = document.getElementById("snsBox").value;
+
+    //呼叫其他檢查function
+    isWrong += checkName(name);
+    isWrong += checkTelephome(tele);
+    isWrong += checkAddress(add);
+    isWrong += checkSNS(sns);
+
+    //資料有錯誤就跳窗提醒
+    console.log("isWrong= " + isWrong);
+
+    if (isWrong > 0) {
+        window.alert("資料有誤 請檢查");
+        console.log("資料有誤");
+    }
+    else {
+        $.ajax({
+            url: '{% url "post-signup" %}',
+            type: "POST",
+            data: { 'name': name, 'phone': tele, 'address': add, 'contact': sns, csrfmiddlewaretoken: '{{ csrf_token }}' },
+            datatype: 'string',
+            success: function (response) {
+                console.log(response);
+                location.href = '{% url "index" %}';
+            }
+        });
+    }
+}
+
+/*
+    checkName
+    確認姓名正確性
+*/
+function checkName(name) {
+    var nameErr = document.getElementById("nameAlert");
+    if (name == "") {
+        nameErr.innerText = "名字不能為空白";
+        return 1;
+    }
+    else {
+        nameErr.innerText = "";
+        return 0;
+    }
+}
+
+/*
+    checkTelephone
+    確認電話正確性
+*/
+function checkTelephome(tele) {
+    var passErr2 = document.getElementById("phoneAlert");
+    var numbers = /^[0-9]+$/;
+    if (tele.match(numbers)) {
+        passErr2.innerText = "";
+        return 0;
+    }
+    else {
+        passErr2.innerText = "請輸入正確的電話號碼";
+        return 1;
+    }
+}
+
+/*
+    checkAddress
+    確認地址的正確
+*/
+function checkAddress(add) {
+    var addErr = document.getElementById("addAlert");
+    if (add == "") {
+        addErr.innerText = "地址不能為空白";
+        return 1;
+    }
+    else {
+        addErr.innerText = "";
+        return 0;
+    }
+}
+
+/*
+    checkSNS
+    確認社群帳號之正確
+*/
+function checkSNS(sns) {
+    var snsErr = document.getElementById("snsAlert");
+    if (sns == "") {
+        snsErr.innerText = "SNS不能為空白";
+        return 1;
+    }
+    else {
+        snsErr.innerText = "";
+        return 0;
+    }
+}
---
+++ static/js/pre-SignUp.js
@@ -0,0 +1,101 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCF_Q4YD_W7FWb40pDU-NHW0ooYsnJWDUM",
+    authDomain: "auction-system-73960.firebaseapp.com",
+    databaseURL: "https://auction-system-73960.firebaseio.com",
+    projectId: "auction-system-73960",
+    storageBucket: "auction-system-73960.appspot.com",
+    messagingSenderId: "650872511305",
+    appId: "1:650872511305:web:8afe3f2f1c33b4f6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+$(".toggle-password").click(function () {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+        input.attr("type", "text");
+    }
+    else {
+        input.attr("type", "password");
+    }
+});
+
+
+/*
+    checkData
+    表單確認後執行
+    確認輸入資料之正確性
+*/
+function checkData() {
+    var isWrong = 0;
+
+    var email = document.getElementById('mailBox').value;
+    var pass = document.getElementById("passBox").value;
+    var pass2 = document.getElementById("passBox2").value;
+    isWrong += checkPassword(pass);
+    isWrong += checkPassword2(pass, pass2);
+    if (isWrong > 0) {
+        window.alert("資料有誤 請檢察");
+    }
+    else {
+        //todo 進行帳號註冊
+        firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+            return;
+        }).then(function () {
+            firebase.auth().currentUser.sendEmailVerification().then(function () {
+                // Email sent.
+                window.alert('Please check verify email.');
+                location.href = '{% url "signin" %}';
+            }).catch(function (error) {
+                // An error happened.
+            });
+        });
+    }
+}
+
+/*
+    checkPassword
+    檢查密碼正確性
+*/
+function checkPassword(pass) {
+    var passErr = document.getElementById("passAlert");
+    var passw = /^[0-9A-Za-z]\w{6,16}$/;
+    if (pass == "") {
+        passErr.innerText = "請輸入密碼";
+        return 1;
+    }
+    if (pass.match(passw)) {
+        passErr.innerText = "";
+        return 0;
+    }
+    else {
+        passErr.innerText = "密碼格式有誤";
+        return 1;
+    }
+}
+
+/*
+    checkPassword2
+    檢查密碼是否輸入正確
+*/
+function checkPassword2(pass, pass2) {
+    var passErr2 = document.getElementById("passAlert2");
+    if (pass == pass2) {
+        passErr2.innerText = "";
+        return 0;
+    }
+    else {
+        passErr2.innerText = "密碼不一致 請重新輸入";
+        return 1;
+    }
+}
+function goSignIn() {
+    location.href = " {% url 'signin' %}";
+}
