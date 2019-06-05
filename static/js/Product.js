ead4f860d5702b01959f9e1ef205b17818ba4b56
im99014 <egg99014@gmail.com>
2019-06-05 20:16:02 +0800

    add produc

--- static/js/Product.js
+++ static/js/Product.js
@@ -1,80 +1,92 @@
 window.onload = load;
 
+// read product's information and show
 function load() {
-    var url = location.href;
-    if(url.indexOf('?')==-1) {
-        alert("無商品!");
-        window.history.back();
-    }
-    else {
-        var array = url.split('?');
-        var id = array[1];
+    $.ajax({
+        url:"/getproductinfo/",
+        cache: false,
+        async: false,
+        success: function(request){
+            // 讀取資料
+            var info = request;
+            // 分別寫入位置
+            document.getElementById("image").setAttribute("src", info.image);
+            document.getElementById("product_name").innerHTML = info.product_name;
+            document.getElementById("category").innerHTML = info.category;
+            document.getElementById("seller").innerHTML = info.seller;
 
-        var str = '';
-        str += '<table><tr><td>圖片:</td><td><img src="';
-        str += info.image
-        str += '"></td></tr><tr><td>名稱:</td><td>';
-        str += info.product_name;
-        str += '</td></tr><td>狀態:</td><td>';
-        
-        if (info.status == 0) {
-            str += '待出售';
+            if (info.status == 0) {
+                document.getElementById("status").innerHTML = "待出售";
+            }
+            else if (info.status == 1) {
+                document.getElementById("status").innerHTML = "競標中";
+            }
+            else if (info.status == 2) {
+                document.getElementById("status").innerHTML = "交易中";
+            }
+            else if (info.status == 3 || info.status == 4) {
+                document.getElementById("status").innerHTML = "已賣出";
+            }
+            else {
+                document.getElementById("status").innerHTML = "已下架";
+            }
+
+            if (info.type == 0) { // 拍賣顯示價格模式
+                document.getElementById("trading_type").innerHTML = "拍賣";
+                document.getElementById("price").innerHTML = info.price + "元";
+                document.getElementById("current_price").innerHTML = info.current_price + "元";
+                document.getElementById("price_per_mark").innerHTML = info.price_per_mark + "元";
+            }
+            else { // 直購顯示價格模式
+                document.getElementById("type").innerHTML = "直購";
+                document.getElementById("price").innerHTML = info.price + "元";
+                document.getElementById("current_price").innerHTML = "--------";
+                document.getElementById("price_per_mark").innerHTML = "--------";
+            }
+
+            document.getElementById("highest_buyer_id").innerHTML = info.highest_buyer_id;
+            document.getElementById("description").innerHTML = info.description;
+            document.getElementById("trading_method").innerHTML = info.trading_method;
+            document.getElementById("deadline").setAttribute("value", info.deadline);
+            // 輸入商品id以上傳
+            var id = document.getElementsByName("id");
+            for (let i = 0; i < 3; i++) {
+                id[i].setAttribute("value", info.id);
+            }
+            // 輸出問與答
+            var question = info.question;
+            var answer = info.answer;
+            var temp = "";
+            for (let i = 0; i < question.length; i++) {
+                temp += "<tr><td>" + question[i] + "</td>";
+                temp += "<td>" + answer[i] + "</td></tr>";
+            }
+            document.getElementById("qa").innerHTML = temp;
         }
-        else if (info.status == 1) {
-            str += '競標中';
-        }
-        else if (info.status == 2) {
-            str += '交易中';
-        }
-        else if (info.status == 3) {
-            str += '已賣出';
-        }
-        else {
-            str += '已下架';
-        }
-        
-        str += '</td></tr><tr><td>直購/拍賣:</td><td>';
-        if (info.type == 0) { // 拍賣
-            str += '拍賣</td></tr><tr><td>直購價:</td><td>';
-            str += info.price;
-            str += '元</td></tr><tr><td>底價:</td><td>';
-            str += info.current_price;
-            str += '元</td></tr><tr><td>每標底價:</td><td>';
-            str += info.price_per_mark;
-        }
-        else { // 直購
-            str += '直購</td><tr><tr><td>直購價:</td><td>';
-            str += info.price;
-            str += '元</td></tr>';
-        }
-        str += '<tr><td>分類:</td><td>';
-        str += info.catagory;
-        str += '</td></tr><tr><td>敘述:</td><td>';
-        str += info.description;
-        str += '</td></tr><tr><td>希望交易方式:</td><td>';
-        str += info.trading_method
-        str += '</td></tr><tr><td>結標日期:</td><td>';
-        str += info.deadline
-        if (canChange == 1) {
-            str += '</td></tr><tr><td></td><td>';
-            str += '<a href="../templates/Modify.html?' + id + '"><input type="button" value="修改"></a>';
-        }
-        else {
-            str += '</td></tr><tr><td></td><td>';
-            str += '<input type="button" onclick="bid()" value="出價">';
-            str += '<input type="button" onclick="buy()" value="直接購買">';
-        }
-        str += '</td></tr></table>';
-        document.getElementById("content").innerHTML = str;
-    }
+    });  
 }
 
-// 出價
-function bid() {
-
+// 送出追蹤請求
+function tracking() {
+    $.ajax({
+        url: "/settrackingproduct/",
+        type: "POST",
+        cache: false,
+        async: false,
+        success: function(){
+            window.alert("追蹤成功！");
+        }
+    })
 }
 
-// 直購
-function buy() {
-
+// 檢查出價
+function checkForm(){
+    window.alert("check");
+    var input = Number(document.getElementById("inputprice").value);
+    if ((input - info.current_price) % info.price_per_mark != 0) {
+        window.alert("出價需為目前價格加上每標底價的整數倍");
+        return false;
+    }
+    window.alert("true");
+    return true;
 }
