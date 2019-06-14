1526492190030b30dea85b54f5049ae38f6d1bb4
Andy <andy870622@gmail.com>
2019-06-14 11:34:38 +0800

    modify bar

--- static/js/bar.js
+++ static/js/bar.js
@@ -87,14 +87,14 @@ function setBody() {
 
   if (!user_status || user_name == "") {
     need =
-      " <header class='header1'><img onclick='index()' style='position:absolute;width:50px;height:50px;margin-left:10px;margin-top:5px;' src='/static/img/ntoulogo.png'><div style='margin-top:15px'><a  onclick='index()' id='sign'>海大拍賣</a><input type=button value='搜尋'  style='font-size:15px;'><input type='text' style='width:100px;'>" +
+      " <header class='header1'><img onclick='index()' style='position:absolute;width:50px;height:50px;margin-left:10px;margin-top:5px;' src='/static/img/ntoulogo.png'><div style='margin-top:15px'><a  onclick='index()' id='sign'>海大拍賣</a><input type=button onclick='searchProduct()' value='搜尋' style='font-size:15px;'><input type='text' style='width:100px;'>" +
       "<span id='bar' class='bar' style='color:white; float: right;'>" +
       "<a onclick='memberCenter()'>會員中心</a><a onclick='openCommand()'>意見回饋</a><a>瀏覽紀錄</a><a onclick='signIn()'> 登入</a><a onclick='signUpString()'> 註冊</a></span></div></header>";
     need2 = "<span id='bar2'>" +
       "<a onclick='memberCenter()'>會員中心</a><a onclick='openCommand()'>意見回饋</a><a>瀏覽紀錄</a><a onclick='signIn()'> 登入</a><a onclick='signUpString()'> 註冊</a></span>";
   } else {
     need =
-      " <header class='header1'><img onclick='index()' style='position:absolute;width:50px;height:50px;margin-left:10px;margin-top:5px;' src='/static/img/ntoulogo.png'><div style='margin-top:15px'><a onclick='index()' id='sign'>海大拍賣</a><input type=button value='搜尋'  style='font-size:15px;'><input type='text' style='width:100px;'>" +
+      " <header class='header1'><img onclick='index()' style='position:absolute;width:50px;height:50px;margin-left:10px;margin-top:5px;' src='/static/img/ntoulogo.png'><div style='margin-top:15px'><a onclick='index()' id='sign'>海大拍賣</a><input onclick='searchProduct()' type=button value='搜尋'  style='font-size:15px;'><input type='text' style='width:100px;'>" +
       "<span id='bar' class='bar' style='color:white; float: right;'>" +
       "<a onclick='memberCenter()'>會員中心</a><a onclick='openCommand()'>意見回饋</a><a>瀏覽紀錄</a><a onclick='signOut()'> 登出</a><a>" + user_name + "</a><span></div></header>";
 
@@ -105,6 +105,24 @@ function setBody() {
   $("#afterBut").after(need2);
   $("body").prepend(need);
 }
+
+function searchProduct()
+{
+  $.ajax(
+    {
+      url:"/searchproduct/",
+      type:'POST',
+      data:{'keyword':keyword},
+      cache:false,
+      async:false,
+      success:function(response)
+      {
+         console.log(response);
+      }
+    }
+  );
+}
+
 //完成loading
 function start() {
   if (isLoading) {
