99aea5b6dcba2fe4425ecdf19c59bd518cdeec65
jasperlin1996 <jasperlin1996@gmail.com>
2019-06-19 21:31:14 +0800

    try Red/Green refactor loop twice : (

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ from django.test import TestCase
 class Test(TestCase):
     def test_index(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 201)
+        print(response)
+        self.assertEqual(response.status_code, 503)
 
 if __name__ == "__main__":
     Test.main()
