258c21c8d79dd5fe698b34f4d8acb02e8e5bfa68
jasperlin1996 <jasperlin1996@gmail.com>
2019-06-19 21:09:58 +0800

    try travis-ci

--- test.py
+++ test.py
@@ -3,5 +3,4 @@ from django.test import TestCase
 class Test(TestCase):
     def test_index(self):
         response = self.client.get('/')
-        
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 201)
